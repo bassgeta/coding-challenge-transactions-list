7b1c41230f91bb3fcc81b362f938c2a2e8691e4b
Bassgeta <kerry.mahne@gmail.com>
2023-10-13 18:10:00 +0200

    fix: link to home

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -40,7 +40,7 @@ const Navigation: React.FC = () => {
         <div className="flex items-center justify-between">
           <a
             className="flex-none text-xl font-semibold dark:text-white"
-            href="."
+            href="/transactions"
           >
             Transactions List
           </a>
